6946cbb29beb5dcc4b9bf16790ff641879289b15
Marco Colombo <mar.colombo13@gmail.com>
2024-11-23 10:09:27 +0100

    Add function to convert from sha1 to colour and reformat message.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use termion::color;
-use termion::color::Color;
 
 fn main() {
-    let size = 8;
-    fill(size, color::Red);
-    fill(size, color::Blue);
-    fill(size, color::Green);
+    let size = 7;
+    let sha1 = "5a98d8";
+    fill(size, &sha1);
 }
 
-fn fill<T: Color>(num: usize, col: T) {
+fn fill(num: usize, sha: &str) {
+    let col = sha2col(sha);
     let fill = " ".repeat(num);
     println!("{}{}{}", color::Bg(col), fill, color::Bg(color::Reset));
+    print!("{}{}{}", color::Bg(col), fill, color::Bg(color::Reset));
+    println!(" Your commit colour is {}", "unknown");
+    println!("{}{}{}", color::Bg(col), fill, color::Bg(color::Reset));
+    println!("#{}", sha);
+}
+
+fn sha2col(sha: &str) -> color::Rgb {
+    let r = u8::from_str_radix(&sha[0..2], 16).unwrap();
+    let g = u8::from_str_radix(&sha[2..4], 16).unwrap();
+    let b = u8::from_str_radix(&sha[4..6], 16).unwrap();
+    color::Rgb(r, g, b)
 }
