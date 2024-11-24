94cf161c3f17369bbbefce0ad8c6df275418c84c
Marco Colombo <mar.colombo13@gmail.com>
2024-11-24 22:11:53 +0100

    Add the Col struct.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 use crossterm::style::{Color, Stylize};
 
+struct Col {
+    r: i32,
+    g: i32,
+    b: i32,
+}
+
+impl Col {
+    fn new(sha: &str) -> Self {
+        Self {
+            r: i32::from_str_radix(&sha[0..2], 16).unwrap(),
+            g: i32::from_str_radix(&sha[2..4], 16).unwrap(),
+            b: i32::from_str_radix(&sha[4..6], 16).unwrap(),
+        }
+    }
+
+    fn rgb(&self) -> Color {
+        Color::Rgb {
+            r: self.r as u8,
+            g: self.g as u8,
+            b: self.b as u8,
+        }
+    }
+}
+
 fn main() {
     let sha1 = "5a98d8";
     fill(sha1, "Unknown");
@@ -7,17 +31,10 @@ fn main() {
 
 fn fill(sha: &str, name: &str) {
     let num = 7;
-    let col = sha2col(sha);
-    let block = " ".repeat(num).on(col);
+    let color = Col::new(sha);
+    let block = " ".repeat(num).on(color.rgb());
     println!("{}", block);
     println!("{} Your commit colour is {}", block, name);
     println!("{}", block);
     println!("#{}", sha);
-}
-
-fn sha2col(sha: &str) -> Color {
-    let r = u8::from_str_radix(&sha[0..2], 16).unwrap();
-    let g = u8::from_str_radix(&sha[2..4], 16).unwrap();
-    let b = u8::from_str_radix(&sha[4..6], 16).unwrap();
-    Color::Rgb { r, g, b }
 }
